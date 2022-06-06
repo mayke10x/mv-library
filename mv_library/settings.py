f548d82f4f80a777dbbb58b215062267dbe0e67d
m.md750.md <m.md750.md@gmail.com>
2022-06-05 23:09:08 -0300

    feat:configurado conexao com banco postgres

--- mv_library/settings.py
+++ mv_library/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'mv_library.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_mvlibrary',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost'
     }
 }
 
