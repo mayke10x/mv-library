b670e1a6b7941a75703564a08e28a834d389a164
m.md750.md <m.md750.md@gmail.com>
2022-06-07 22:27:03 -0300

    feat:incluido as rotas para fornecer a api

--- mv_library/urls.py
+++ mv_library/urls.py
@@ -1,9 +1,15 @@
-from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
+
+from rest_framework import routers
+from books import views
+
+router = routers.DefaultRouter()
+router.register(r'books', views.BookViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)   
+
+    path('', include(router.urls))
 ] + static('/', document_root='')
