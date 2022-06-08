fc5a9bcf44e84484d8d804e60b4a026a4382b720
m.md750.md <m.md750.md@gmail.com>
2022-06-07 22:23:22 -0300

    feat:adicionado nos apps o rest_framework
    - Adicionado nos apps o rest_framework para da inicio a criacao da api;

--- mv_library/settings.py
+++ mv_library/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'books'
+    'rest_framework',
+    'books',
 ]
 
 MIDDLEWARE = [
