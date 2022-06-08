5f613017e6d983c6f057b899dbbeccb14382f934
m.md750.md <m.md750.md@gmail.com>
2022-06-07 22:20:17 -0300

    refac:mensagem erro de validacao da data public.
    - Refatorado mensagem de erro de validacao do campo de data da
    publicacao;
    - Criado um bloco para formatar o nome do arquivo de capa do livro
    enviada;

--- books/models.py
+++ books/models.py
@@ -4,26 +4,32 @@ from django import forms
 
 from django.utils.html import mark_safe
 
+# Valida a data de publicação onde tem de ser maior ou igual a 1500 e
+# menor ou igual a 2022
+def valideDate (value):
+    if value.year < 1500 or value.year > 2022:
+        raise forms.ValidationError('A data de publicação do livro deve está compreendida entre os anos de 1500 a 2022')
+
+# Formata o nome do arquivo
+# da capa do livro que for enviada
+def format_namefile_capa(self, filename):
+    return f"{self.nome}-{filename}"
+
 class Book(models.Model):
     nome = models.CharField(max_length=100, unique=True, blank=False)
     categoria = models.CharField(max_length=50, blank=False)
     autor = models.CharField(max_length=125, blank=False)
-    capa_do_livro = models.ImageField()
-    data_de_publicacao = models.DateField(default=datetime.now, blank=False)
+    capa_do_livro = models.ImageField(upload_to=format_namefile_capa)
+    data_de_publicacao = models.DateField(default=datetime.now, blank=False, validators=[valideDate])
     quantidade_de_paginas = models.IntegerField(blank=False)
-
-    # Valida a data de publicação onde tem de ser maior ou igual a 1500 e
-    # menor ou igual a 2022
-    def __str__ (self):
-        if self.data_de_publicacao.year >= 1500 and self.data_de_publicacao.year <= 2022:
-            return str(self.data_de_publicacao)
-        else:
-            raise forms.ValidationError('A data de publicação deve ser expedida entre os anos de 1500 e 2022')
 
     # Devolve a imagem para visualização na área de edição ou
     # visualização de um item
     def image_tag(self):
-        return mark_safe('<img src="/%s" width="150" height="150" />' % (self.capa_do_livro))
+        if self.capa_do_livro:
+            return mark_safe('<img src="/%s" width="150" height="150" />' % (self.capa_do_livro))
+        else:
+            return str('-')
 
     image_tag.short_description = 'Image'
                 
